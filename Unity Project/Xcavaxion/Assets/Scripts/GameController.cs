d17619b721a5584338b1b0c09ed956c1139c9f8c
Alex White <lide128@gmail.com>
2017-10-10 15:55:26 -0500

    Update get ignore file name
    Seem to be working now, ignoring Library and Temp and userpref files

--- Unity Project/Xcavaxion/Assets/Scripts/GameController.cs
+++ Unity Project/Xcavaxion/Assets/Scripts/GameController.cs
@@ -1,16 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
+	public GameSetupParameters parameters; 
+
+	public MapController currentGameMap;
+
+	public bool gameWon;
+
+	public int boulderCount;
+	public int elementBoxCount;
+
 	// Use this for initialization
 	void Start () {
-		
+
+		gameWon = false;
+		boulderCount = 0;
+		elementBoxCount = 0;
+
+		//Load menu screen parameters in to mapcontroller?
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		GetBoulderState ();
+		GetElementBoxState ();
+
+		if(boulderCount == 0 && elementBoxCount == 0){
+			EndGame ();
+		}
 	}
+
+	public void GetBoulderState(){
+		boulderCount = currentGameMap.onScreenBoulderCount;
+	}
+
+	public void GetElementBoxState(){
+		elementBoxCount = currentGameMap.onScreenElementBoxCount;
+	}
+
+	public void EndGame(){
+		SceneManager.LoadScene(0);
+	}
+
 }
